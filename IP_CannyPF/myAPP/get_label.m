f553ab43efee5aa904e6628baa562d97c03b3440
DAI Dinggen <942856733@qq.com>
2022-09-16 21:13:06 +0200

    Up to date

--- IP_CannyPF/myAPP/get_label.m
+++ IP_CannyPF/myAPP/get_label.m
@@ -4,6 +4,8 @@ function [label_left_border, label_right_border] = get_label(label_data, folderN
 %   [label_left_border, label_right_border] = get_label(label_data, 'x2022_04_28_13_09_34', 'x38169360000')
 % data.x2022_04_28_13_09_34.x35424000 -> label: [683, 2132]
 %   [label_left_border, label_right_border] = get_label(label_data, 'x2022_04_28_13_09_34', 'x35424000')
+folderName = ['x', folderName]; % there is extra 'x' at the begin of the folder and image name when the JSON file is loaded.
+imageName = ['x', imageName];
 
 if ~isempty(getfield(label_data, folderName, imageName))
     value = getfield(label_data, folderName, imageName, 'coords');
---
+++ IP_CannyPF/temp_test.m
@@ -0,0 +1,11 @@
+R=35; G = 8; B=6;
+R*0.299 + G*0.587 + B*0.114
+%% only for test
+
+figure(2);
+imshow(img);
+hold on
+for i = 1:1604
+    edge_1 = edgeChains(edgeChains(:, 3) == i, :);
+    plot(edge_1(:, 1), edge_1(:, 2), 'color', 'green')  
+end
